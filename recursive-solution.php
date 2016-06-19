7aec8a339b483f245ad11da636aede0c9d26415e
h4d4 <h4d4@h4d4>
2016-06-18 21:36:49 -0500

    clear function to clear query_list for each testcase

--- recursive-solution.php
+++ recursive-solution.php
@@ -8,13 +8,25 @@ function read () {
   $arr2 = explode(" ", $arr[0]);
   return $arr2;
 }
- 
+
+function clear (&$query_list) {
+	$query_list = array();
+}
+
 function update_process ($x,  $y, $z, $val, &$query_list) {
 	$key = (string)$x . ";" . (string)$y . ";" . (string)$z;
 	print($key);
 	$query_list [$key] = $val;
 }
 
+function query_process ( $x1, $y1, $z1, $x2, $y2, $z2, &$query_list) {
+	
+	for($i = 0; $i < 1000; $i++) {
+
+
+	}
+
+}
 
 
 function main(){
@@ -23,6 +35,8 @@ function main(){
   $TEST = (int)$in[0];
 
   for ($i = 0; $i < $TEST; $i++) {
+    
+  	clear($query_list);
 
     $in = read();
     $n = (int)$in[0];
@@ -47,7 +61,7 @@ function main(){
         $y2 = (int)$query[5];
         $z2 = (int)$query[6];
 
-        //query_process(); //
+        query_process( $x1, $y1, $z1, $x2, $y2, $z2, $query_list);
       }
     }
   }
